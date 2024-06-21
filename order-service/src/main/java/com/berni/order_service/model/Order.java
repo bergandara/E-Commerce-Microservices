f511e8d342bd667cfde7d0f3d182415e81e03c58
bergandara <gandaraber@gmail.com>
2024-06-21 12:14:15 +0100

    Order and OrderLineItems classes created

--- order-service/src/main/java/com/berni/order_service/model/Order.java
+++ order-service/src/main/java/com/berni/order_service/model/Order.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Entity
 @Table(name="t_orders")
 @Getter
@@ -17,5 +19,6 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String orderNumber;
+    @OneToMany(cascade = CascadeType.ALL)
     private List<OrderLineItems> orderLineItemsList;
 }

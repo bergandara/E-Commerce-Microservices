63d248dd9775695bb0b7d099b9d67930c183fd94
bergandara <gandaraber@gmail.com>
2024-06-22 00:16:28 +0100

    Controller updated and defaultInventory created in main

--- inventory-service/src/main/java/com/berni/inventory_service/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/berni/inventory_service/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package com.berni.inventory_service;
 
+import com.berni.inventory_service.model.Inventory;
+import com.berni.inventory_service.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +14,12 @@ public class InventoryServiceApplication {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("iphone_13");
+			inventory.setQuantity(100);
+		}
+	}
 }

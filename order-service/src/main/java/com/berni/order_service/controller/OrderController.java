9fd92317a7daf281ab0e8a529f5b26b31f25945d
bergandara <gandaraber@gmail.com>
2024-06-21 12:26:40 +0100

    Update OrderController.java

--- order-service/src/main/java/com/berni/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/berni/order_service/controller/OrderController.java
@@ -1,14 +1,22 @@
 package com.berni.order_service.controller;
 
 import com.berni.order_service.dto.OrderRequest;
+import com.berni.order_service.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
+@RequiredArgsConstructor
 public class OrderController {
 
+    private final OrderService orderService;
+
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public String placeOrder(@RequestBody OrderRequest orderRequest){
+        orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
     }
 }

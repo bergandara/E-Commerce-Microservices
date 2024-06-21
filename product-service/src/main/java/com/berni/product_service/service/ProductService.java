428ce9049680932a983da8de3b008bfe1d4f1637
bergandara <gandaraber@gmail.com>
2024-06-21 11:27:26 +0100

    getAllProducts() method added

--- product-service/src/main/java/com/berni/product_service/service/ProductService.java
+++ product-service/src/main/java/com/berni/product_service/service/ProductService.java
@@ -1,12 +1,15 @@
 package com.berni.product_service.service;
 
 import com.berni.product_service.dto.ProductRequest;
+import com.berni.product_service.dto.ProductResponse;
 import com.berni.product_service.model.Product;
 import com.berni.product_service.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
@@ -25,5 +28,20 @@ public class ProductService {
 
         productRepository.save(product);
         log.info("Product {} is saved", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
     }
 }
